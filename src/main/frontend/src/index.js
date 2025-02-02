cda61092534f5581ec3cd974a34e5ae82fe574aa
gio250512 <giovanni_1995@hotmail.com>
2025-02-02 02:05:21 -0700

    Revert "Merge branch 'main' of https://github.com/gio250512/cs320-project-giovannimedina"
    This reverts commit 93bb4ca70de58e1cba9ff92f4131f2001149a61e, reversing
    changes made to 75e945bdad4d32cbc4e5b89ed7d273499150cb6d.

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './Components/Pages/Home';
-import SignInPage from './Components/Pages/SignInPage'
+import App from './Components/App';
+import Page2 from './Components/Page2'
 import Navbar from './Components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Amplify } from 'aws-amplify';
@@ -17,8 +17,8 @@ root.render(
         <BrowserRouter>
             <Navbar/>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/SignInPage" element={<SignInPage />} />
+                <Route path="/" element={<App />} />
+                <Route path="/page2" element={<Page2 />} />
                 {/* You can add more pages by adding more Route elements here */}
             </Routes>
         </BrowserRouter>
